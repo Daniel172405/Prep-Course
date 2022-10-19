1a1c4cf9669561ea86a3afc6ae1b61a1a61fe056
unknown <danielramirez2405@gmail.com>
2022-10-19 08:16:53 -0300

    hola

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,11 +1,10 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = "hddfgssa";
-
+const nuevaString = "2";
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = null;
+const nuevoNum = ;
 
 // Crea una variable booleana:
 const nuevoBool = null;
@@ -16,9 +15,7 @@ const nuevaResta = 10 - null === 5;
 // Resuelve el siguiente problema matemático:
 const nuevaMultiplicacion = 10 * null === 40 ;
 
-// Resuelve el siguiente problema matemático:'git help -a' and 'git help -g' list available subcommands and some
-
- 
+// Resuelve el siguiente problema matemático:
 const nuevoModulo = 21 % 5 === null;
 
 
